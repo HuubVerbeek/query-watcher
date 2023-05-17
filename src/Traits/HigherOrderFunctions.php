88df9daa496a4bbe79b249bb223c9a436b7604bd
Huub Verbeek <h.verbeek@brisp.nl>
2023-05-17 21:15:39 +0200

    m/hv/update_comments

--- src/Traits/HigherOrderFunctions.php
+++ src/Traits/HigherOrderFunctions.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Collection;
 trait HigherOrderFunctions
 {
     /**
-     * Map empty bindings to enum
+     * Map empty bindings to enum closure
      *
      * @return Closure
      */
